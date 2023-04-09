5f092dd163b640fe86d0dce59d7e97d49471bf93
rm9046 <rm9046612@gmail.com>
2023-04-10 01:04:23 +0800

    1

---
+++ app/src/main/java/com/opentv/player/ActivityBridge.java
@@ -0,0 +1,14 @@
+package com.opentv.player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityBridge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bridge);
+    }
+}
