0706aa01b4bb0e59bec227791b0a34831c35572a
rm9046 <rm9046612@gmail.com>
2023-04-10 18:18:12 +0800

    1

--- app/src/main/java/com/opentv/player/ActivityBridge.java
+++ app/src/main/java/com/opentv/player/ActivityBridge.java
@@ -2,13 +2,46 @@ package com.opentv.player;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Set;
 
 public class ActivityBridge extends AppCompatActivity {
+    private static final String TAG = "ActivityBridge";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bridge);
+    }
+
+    @Override
+    protected void  onStart(){
+        super.onStart();
+        aaa();
+    }
+
+    void aaa(){
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();  //
+        String action = intent.getAction();
+        String scheme = intent.getScheme();
+        Set<String> categories = intent.getCategories();
+        Log.e(TAG, "data==========="+data);
+        Log.e(TAG, "action==========="+action);
+        Log.e(TAG, "categories==========="+categories);
+        Log.e(TAG, "DataString==========="+intent.getDataString());
+        Log.e(TAG, "==============================");
+        Log.e(TAG, "scheme==========="+scheme);
+        Log.e(TAG, "id ==========="+data.getQueryParameterNames());
+        Log.e(TAG, "host==========="+data.getHost());
+        Log.e(TAG, "path==========="+data.getPath());
+        Log.e(TAG, "port==========="+data.getPort());
+
+
     }
 }
--- app/src/main/java/com/opentv/player/ActivityMain.java
+++ app/src/main/java/com/opentv/player/ActivityMain.java
@@ -1,6 +1,9 @@
 package com.opentv.player;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,7 +15,10 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.opentv.player.databinding.ActivityMainBinding;
 
+import java.util.Set;
+
 public class ActivityMain extends AppCompatActivity {
+    private static final String TAG = "ActivityMain";
 
     private ActivityMainBinding binding;
 
@@ -32,6 +38,37 @@ public class ActivityMain extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        aaa();
+    }
+
+    void aaa() {
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();  //
+        String action = intent.getAction();
+        String scheme = intent.getScheme();
+        Set<String> categories = intent.getCategories();
+        Log.e(TAG, "data===========" + data);
+        Log.e(TAG, "action===========" + action);
+        Log.e(TAG, "categories===========" + categories);
+        Log.e(TAG, "DataString===========" + intent.getDataString());
+        Log.e(TAG, "==============================");
+        Log.e(TAG, "scheme===========" + scheme);
+        Log.e(TAG, "id ===========" + data.getQueryParameterNames());
+        Log.e(TAG, "host===========" + data.getHost());
+        Log.e(TAG, "path===========" + data.getPath());
+        Log.e(TAG, "port===========" + data.getPort());
+
+        String host = data.getHost().trim();
+
+
     }
 
 }
